google-mirror/build_240522

Refactor zipalign to allow unit tests
Test: Self-tested
Bug: NA
Change-Id: If5e42735853e1508fbeb9fb420a9a9f129c2218f

tools/zipalign/tests/src/align_test.cpp
@@ -1,0 +1,15 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "ZipAlign.h"
+
+#include <stdio.h>
+
+using namespace android;
+
+TEST(Align, AlignZipWithHoles) {
+  const char* src = "tests/data/zip_with_holes.zip";
+  const char* dst = "tests/data/test_align_with_holes.zip.tmp";
+  int result = process(src, dst, 4, true, false, 4096);
+  ASSERT_EQ(0, result);
+}

AERA-Recovery/android_build_make

tools/zipalign/tests/src/align_test.cpp
@@ -1,0 +1,15 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "ZipAlign.h"
+
+#include <stdio.h>
+
+using namespace android;
+
+TEST(Align, Unaligned) {
+  const char* src = "tests/data/unaligned.zip";
+  const char* dst = "tests/data/unaligned_out.zip";
+  int result = process(src, dst, 4, true, false, 4096);
+  ASSERT_EQ(0, result);
+}