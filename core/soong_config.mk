google-mirror/build_240522

Add PRODUCT_SHIPPING_API_LEVEL to productVariables
Bug: 175526482
Test: m selinux_policy
Change-Id: I10195730e47398bdb455d554a1cb7d6b7ac28634

core/soong_config.mk
@@ -226,6 +226,8 @@ $(call add_json_list, BoardKernelModuleInterfaceVersions, $(BOARD_KERNEL_MODULE_
 
 $(call add_json_bool, BoardMoveRecoveryResourcesToVendorBoot, $(BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT))
 
+$(call add_json_int,  ShippingApiLevel, $(PRODUCT_SHIPPING_API_LEVEL))
+
 $(call json_end)
 
 $(file >$(SOONG_VARIABLES).tmp,$(json_contents))

AERA-Recovery/android_build_make

core/soong_config.mk
@@ -239,6 +239,8 @@ $(call add_json_list, BoardKernelModuleInterfaceVersions, $(BOARD_KERNEL_MODULE_
 $(call add_json_bool, BoardMoveRecoveryResourcesToVendorBoot, $(BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT))
 $(call add_json_str,  PrebuiltHiddenApiDir, $(BOARD_PREBUILT_HIDDENAPI_DIR))
 
+$(call add_json_str,  ShippingApiLevel, $(PRODUCT_SHIPPING_API_LEVEL))
+
 $(call json_end)
 
 $(file >$(SOONG_VARIABLES).tmp,$(json_contents))