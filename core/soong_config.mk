google-mirror/build_240522

Export some make variables to soong.variables.
Bug: 275472038
Test: CIs
Change-Id: I2e7a07007224364418659453d979e2bee58fe5f9

core/soong_config.mk
@@ -315,6 +315,12 @@ $(call add_json_list, SourceRootDirs,             $(PRODUCT_SOURCE_ROOT_DIRS))
 
 $(call add_json_list, AfdoProfiles,                $(ALL_AFDO_PROFILES))
 
+$(call add_json_str, ProductManufacturer,         $(PRODUCT_MANUFACTURER))
+$(call add_json_str, ProductBrand,                $(PRODUCT_BRAND))
+$(call add_json_str, BuildFingerprintBuildNumber, $(BF_BUILD_NUMBER))
+$(call add_json_str, TargetBuildVariant,          $(TARGET_BUILD_VARIANT))
+$(call add_json_str, BuildVersionTags,            $(BUILD_VERSION_TAGS))
+
 $(call json_end)
 
 $(file >$(SOONG_VARIABLES).tmp,$(json_contents))

AERA-Recovery/android_build_make

(cherry picked from https://android-review.googlesource.com/q/commit:cbffd5c537ac0c1e02ab1faf311e2a9017cdcf00)
Merged-In: I2e7a07007224364418659453d979e2bee58fe5f9

core/soong_config.mk
@@ -323,6 +323,10 @@ $(call add_json_list, SourceRootDirs,             $(PRODUCT_SOURCE_ROOT_DIRS))
 
 $(call add_json_list, AfdoProfiles,                $(ALL_AFDO_PROFILES))
 
+$(call add_json_str,  ProductManufacturer, $(PRODUCT_MANUFACTURER))
+$(call add_json_str,  ProductBrand,        $(PRODUCT_BRAND))
+$(call add_json_list, BuildVersionTags,    $(BUILD_VERSION_TAGS))
+
 $(call json_end)
 
 $(file >$(SOONG_VARIABLES).tmp,$(json_contents))